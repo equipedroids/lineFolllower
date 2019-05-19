2e5e758f87af335a89bf6f96adf313f87d9eab7e
gigaDroid-Daniel <oficial.danieloliveira@gmail.com>
2019-05-19 19:28:53 -0300

    Atualizei aqui com o PID

--- lineFolllower.ino
+++ lineFolllower.ino
@@ -1,78 +1,85 @@
+/* LIBRARIES */
+// Falcon library
 #include <FalconRobot.h>
 
+/* CLASSES */
+// Class to make robot drives straight
+class motorPID{
+  private:
+    float Kp = 5, Ki = 0, Kd = 0;
+    //float Kp = 0.7, Ki = 0.5, Kd = 0.9;
+    float proportional, integral, derivative;
+    int error, lastError = 0, sumError = 0;
+  public:
+    int motorSpeed;
+    int motorDirection;
+    void getSpeed(int actualLight, int defaultLight){
+      error = actualLight - defaultLight;
+      proportional = error * Kp;
+      sumError += error;
+      integral = sumError * Ki;
+      derivative = (error - lastError) * Kd;
+      lastError = error;
+      motorSpeed = proportional + integral + derivative;
+    }
+    void getDirection(){
+      if(motorSpeed < 0){
+        motorSpeed *= -1;
+        motorDirection = BACKWARD;
+      }else{
+        motorDirection = FORWARD;
+      }
+    }
+};
+
+/* VARIABLES */
 // Motors
 FalconRobotMotors motors(5, 7, 6, 8);
 // Line sensors
 FalconRobotLineSensor leftLightSensor(A2);
 FalconRobotLineSensor rightLightSensor(A3);
 // Default values for line sensors
-#define leftInLine 1003
-#define leftOutOfLine 911
-#define rightInLine 1004
-#define rightOutOfLine 926
-// Variables for right motor
-int rightLight;
-int rightSumError = 0;
-int rightLastError = 0;
-int rightSpeed = 0;
-int rightDirection = 0;
-// Variables for left motor
-int leftLight;
-int leftSumError = 0;
-int leftLastError = 0;
-int leftSpeed = 0;
-int leftDirection = 0;
+#define LEFT_INLINE 1005
+#define LEFT_OUTLINE 930
+#define RIGHT_INLINE 1010
+#define RIGHT_OUTLINE 945
+#define DEFAULT_SPEED 250
+// Line sensors variables
+int rightLightValue, leftLightValue;
+// PID variables
+motorPID *rightMotor;
+motorPID *leftMotor;
 
+/* INITIAL FUNCTION */
 void setup() {
   Serial.begin(9600);
-  delay(1000);
+  delay(5000);
+
+  rightMotor = new motorPID();
+  leftMotor = new motorPID();
 }
 
-int getSpeed(int light, int defaultLight, int sumError, int lastError){
-  float Kp = 0.3;
-  float Ki = 0.3;
-  float Kd = 0.3;
-  float proporcional;
-  float integral;
-  float derivada;
-  int error;
-  float motorSpeed;
-  // int sumError;
-  // int lastError;
-  error = light - defaultLight;
-  proporcional = error * Kp;
-  integral = sumError * Ki;
-  derivada = (error - lastError) * Kd;
-  motorSpeed = proporcional + integral + derivada;
-  lastError = error;
-  return motorSpeed;
+/* FUNCTION TO VERIFY IF ROBOT IS IN LINE */
+bool isInLine(){
+  if(rightLightValue <= RIGHT_INLINE-30 && leftLightValue <= LEFT_INLINE-30){ return false; }
+  else{ return true; }
 }
 
+/* MAIN FUNCTION */
 void loop() {
   // Line sensor reading
-  leftLight = leftLightSensor.read();
-  rightLight = rightLightSensor.read();
+  leftLightValue = leftLightSensor.read();
+  rightLightValue = rightLightSensor.read();
   
-  if(leftLight <= leftInLine-30 && rightLight <= rightInLine-30){
-    //Serial.println("Fora da linha");
-    Serial.print(rightLight);
-    Serial.print("\t");
-    Serial.println(leftLight);
-    motors.drive(40, FORWARD);
-  }else{
-    //Serial.println("Na linha");
-    Serial.print(rightSpeed);
-    Serial.print("\t");
-    Serial.println(leftSpeed);
-    rightSpeed = getSpeed(rightLight, rightOutOfLine, rightSumError, rightLastError);
-    leftSpeed = getSpeed(leftLight, leftOutOfLine, leftSumError, leftLastError);
-    
-    if(rightSpeed < 0){ rightSpeed *= -1; rightDirection = BACKWARD; }
-    else { rightDirection = FORWARD; }
-    if(leftSpeed < 0){ leftSpeed *= -1; leftDirection = BACKWARD; }
-    else { leftDirection = FORWARD; }
-    
-    motors.rightDrive(rightSpeed, rightDirection);
-    motors.leftDrive(leftSpeed, leftDirection);
+  if(!isInLine()){
+    motors.drive(DEFAULT_SPEED, FORWARD);
+  }
+  else{
+    rightMotor->getSpeed(rightLightValue, RIGHT_OUTLINE);
+    rightMotor->getDirection();
+    motors.rightDrive(rightMotor->motorSpeed, rightMotor->motorDirection);
+    leftMotor->getSpeed(leftLightValue, LEFT_OUTLINE);
+    leftMotor->getDirection();
+    motors.leftDrive(leftMotor->motorSpeed, leftMotor->motorDirection);
   }
 }
